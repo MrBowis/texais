544cc8edc553995349ca78fba10a8a1395e14737
MrBowis <02alejo20@gmail.com>
2025-08-21 01:35:15 -0500

    deploy in AWS EC2

--- texais-frontend-latest/src/app/services/routes.ts
+++ texais-frontend-latest/src/app/services/routes.ts
@@ -1,5 +1,5 @@
 export enum  Routes {
-    API_END_POINT = 'http://localhost:8000/api/pdf-handler',
+    API_END_POINT = 'http://3.144.162.249:8000/api/pdf-handler',
     MERGE = '/merge/',
     BLOCK = '/secure/block/',
     UNBLOCK = '/secure/unblock/',
